7d57ca59fa5bddcad87565baa21639186bf8a332
alfimiftanur <alfinurhakim1234@gmail.com>
2025-02-13 14:38:26 +0700

    menambahkan latihan2a dan 2b

---
+++ pertemuan2/latihan2a.java
@@ -0,0 +1,25 @@
+package pertemuan2;
+
+public class latihan2a {
+    public static void main(String[] args) {
+        
+        Node n1 = new Node(5);
+        Node n2 = new Node(7);
+        Node n3 = new Node(9);
+        Node n4 = new Node(8);
+
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+
+        Node p = n1;
+        while (p != null) {
+            System.out.print(p.getNilai());
+            if (p.getNext() != null) { 
+                System.out.print(", ");
+        }
+        p = p.getNext();
+    }
+    }
+}
+
