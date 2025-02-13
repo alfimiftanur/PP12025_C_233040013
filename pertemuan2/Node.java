9312f1abc073219c4b06a0f5a8cfc66864b49366
alfimiftanur <alfinurhakim1234@gmail.com>
2025-02-13 11:01:16 +0700

    pertemuan 2

---
+++ pertemuan2/Node.java
@@ -0,0 +1,27 @@
+public class Node {
+    private int nilai;
+    private Node next;
+
+    public Node (int nilai){
+        this.nilai = nilai;
+    }
+
+    // getter & setter
+
+    public void setNilai (int nilai) {
+        this.nilai = nilai;
+    }
+       
+    public int getNilai () {
+        return nilai;
+    }
+
+    public void setNext (Node next){
+        this.next = next;
+    }
+
+    public Node getNext (){
+        return next;
+    }
+    
+}
